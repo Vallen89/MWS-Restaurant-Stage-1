e6514caab6ec9f9f7b39b63c622edb9a59ba96b7
Vallen89 <33203410+Vallen89@users.noreply.github.com>
2018-05-11 21:28:01 +0200

    Fetch Event installed in SW

--- js/sw.js
+++ js/sw.js
@@ -45,4 +45,17 @@ self.addEventListener('activate', function(event) {
     })
   );
 });
-//
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
